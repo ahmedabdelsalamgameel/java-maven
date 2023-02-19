4696ef5d257b7a8f1c2f55e9a44d9ac46a7428f5
ahmedabdelsalam <ahmedabdelsalamgameel1032@gmail.com>
2023-02-20 01:41:19 +0200

    update jenkinsfile+script.groovy

--- script.groovy
+++ script.groovy
@@ -1,19 +1,26 @@
 def buildJar() {
-    echo "building the application..."
-    sh 'mvn package'
+    echo "<- Awesome, you are in build jar stage ->"
+    sh "mvn package"
 } 
 
 def buildImage() {
-    echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanajanashia/demo-app:jma-2.0 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
-        sh 'docker push nanajanashia/demo-app:jma-2.0'
+    echo "<- Good work ,  this is buildImage stage ->"
+    // --> add more steps here for docker login <--
+    // here another way to use credentials  "wrapper syntax" way
+    withCredentials([
+        usernamePassword(credentials: 'Docker_repo',
+                            usernameVariable: USERNAME ,
+                            passwordVariable: PASSWORD)
+    ])
+    {
+        sh " docker build -t ahmedabdelsalam19/test-app:javaMaven-2.0 . "
+        sh " echo $PASSWORD | docker login -u $USERNAME --password-stdin "
+        sh " docker push ahmedabdelsalam19/test-app:javaMaven-2.0 "
     }
 } 
 
 def deployApp() {
-    echo 'deploying the application...'
+    echo "<- thanks to reach here, this is deploy stage ->"
 } 
 
 return this
